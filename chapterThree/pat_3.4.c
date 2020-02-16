42df0b9a5646361001379f4839ac6d1702b48e45
Aaron1028 <3459976576@qq.com>
2020-02-16 20:19:25 +0800

    2020-2-16

--- chapterThree/pat_3.4.c
+++ chapterThree/pat_3.4.c
@@ -26,37 +26,45 @@ List getValue(List L, int n){
     return p;
 }
 
-List getMaxLength(List L, int n){
-    List MaxLength;
-    MaxLength ->Length = 0;
-    int head = 0, tail = 1, length = 1;
-    int count = 0;
+void getMaxLength(List L, int n){
     List p = L;
-    if(p ->Data[head] < p ->Data[tail]){
-        head++;
-        tail++;
-        length++;
-    } else{
-        if(length > MaxLength ->Length){
-            for (int j = tail - length; j <= length ; j++) {
-                int k = 0;
-                MaxLength->Data[k] = p->Data[j];
-                MaxLength->Length++;
-                k++;
+    int l = 0, r = 0, len = 1;
+    int maxL = 0, maxR = 0, maxLen = 1;
+    for (int i = 1; i < n; ++i) {
+        if(p ->Data[i] > p ->Data[i-1]){
+            r++;
+            len++;
+        }
+        else{
+            if(len > maxLen){
+                maxL = l;
+                maxR = r;
+                maxLen = len;
             }
+            l = r = i;
+            len = 1;
         }
     }
-    for (int i = 0; i < length; ++i) {
-        printf("%d ",MaxLength ->Data[i]);
+    if(len > maxLen){
+        maxL = l;
+        maxR = r;
+        maxLen = len;
     }
+    for (int j = maxL; j <= maxR; ++j) {
+        if(j == maxR){
+            printf("%d", p->Data[j]);
+        }else{
+            printf("%d ", p->Data[j]);
+        }
 
+    }
 }
 
- int main(){
+int main(){
     int n;//顺序表长度
     scanf("%d", &n);
     List list = Init();
     list = getValue(list,n);
-    getValue(list,n);
+    getMaxLength(list,n);
     return 0;
- }
+}
